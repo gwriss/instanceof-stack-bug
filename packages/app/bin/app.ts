9ace8d5043ac73df3b99ad2e3b3a85ee546aba6e
Tobias Bardino <tobias@bardino.dk>
2020-10-02 20:18:37 +0200

    Create failing test with external stack

--- packages/app/bin/app.ts
+++ packages/app/bin/app.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { AppStack } from '../lib/app-stack';
+import { ExternalStack } from 'external/lib/external-stack';
 
 const app = new cdk.App();
 new AppStack(app, 'AppStack');
+new ExternalStack(app, 'ExternalStack');
--- packages/app/test/app.test.ts
+++ packages/app/test/app.test.ts
@@ -1,13 +1,26 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as App from '../lib/app-stack';
+import * as External from 'external/lib/external-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new App.AppStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new App.AppStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(matchTemplate({
+    "Resources": {}
+  }, MatchStyle.EXACT));
+});
+
+test('SynthUtils local stack', () => {
+  const app = new cdk.App();
+  const stack = new App.AppStack(app, 'MyTestStack');
+  SynthUtils.toCloudFormation(stack);
+});
+
+test('SynthUtils external stack', () => {
+  const app = new cdk.App();
+  const stack = new External.ExternalStack(app, 'MyTestStack');
+  SynthUtils.toCloudFormation(stack);
 });
